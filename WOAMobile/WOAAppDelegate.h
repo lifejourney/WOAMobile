8f8dc914897fa7cb44d9f5475d1e9c78ea9f4030
Steven Zhuang <lifejourney@126.com>
2014-05-31 21:29:41 +0800

    Initial Commit

---
+++ WOAMobile/WOAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WOAAppDelegate.h
+//  WOAMobile
+//
+//  Created by steven.zhuang on 5/31/14.
+//  Copyright (c) 2014 steven.zhuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WOAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WOAMobileTests/WOAMobileTests.m
@@ -0,0 +1,34 @@
+//
+//  WOAMobileTests.m
+//  WOAMobileTests
+//
+//  Created by steven.zhuang on 5/31/14.
+//  Copyright (c) 2014 steven.zhuang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WOAMobileTests : XCTestCase
+
+@end
+
+@implementation WOAMobileTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
